53937b464ca4945510f12c6ad97ff7f5a00f58e7
Shaswat Raj <66713844+SH20RAJ@users.noreply.github.com>
2021-03-10 12:45:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,26 @@
 <?php
-$img = "";
-$html="";
+$html = "";
+//Creating Variable
+
+//Getting Json File From URL?__a=1
 if(isset($_GET['url'])) {
-    $json = file_get_contents($_GET['url']."?__a=1") ;
+    $json = file_get_contents($_GET['url']."?__a=1");
+    //Getting the file content
     $json = json_decode($json);
-    $img = $json->graphql->shortcode_media->display_url;
-   // print_r();
-    $aar = $json->graphql->shortcode_media->display_resources ;
-    
-    for($i=0;$i<count($aar);$i++ ) {
-        $html .= '<img src="'.$aar[$i]->src.'" > <br><br> <a href="'.$aar[$i]->src.'" download >Download</a><hr>';
+    //Converting the JSON into Php object
+
+    //Let's get the images into $html and image array to $arr... 
+    $arr = $json->graphql->shortcode_media->display_resources;
+
+    //we want array[index].src 
+    //Let's do using for loop and store into $html variable
+
+    for($i=0;$i<count($arr);$i++ ) {
+        $html .= '<img src="'.$arr[$i]->src.'" > <br><br> <a href="'.$arr[$i]->src.'" download >Download</a><hr>';
     }
-} else {
-    //
+    //we got this Let's Use Some CSs
+
+    //Source Code in Description
 }
 
 ?>
@@ -22,17 +30,17 @@ if(isset($_GET['url'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>InstaxDownload</title>
+    <title>Instagram Photo Downloader</title>
 </head>
 <body>
     <h1>Instax - Instagram Downloader</h1>
     <form action="" method="get">
-        <input type="text" name="url" id="" placeholder="https://www.instagram.com/p/CMM9uxALsjc/">
-        <button type="submit">GRAB</button>
+    <input type="text" name="url" id="">
+    <button type="submit">GRAB</button>
     </form>
 
-    <div class="images">
-       <?php echo $html ; ?> 
+    <div class="image">
+    <?php echo $html ; //Showing all Stored Images ?>
     </div>
     <style>
         *{
@@ -63,6 +71,5 @@ if(isset($_GET['url'])) {
         form {
         }
     </style>
-    
 </body>
 </html>
